c1940429bc2e311ad2b8b29cf2e9daaa1b120af1
Maddy Sutton <msutton@bsd220.org>
2018-11-01 11:20:05 -0400

    Basic movement

---
+++ Assets/scripts/ballBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(0f,0f,0.2f);
+		}
+		
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(-0.2f,0f,0f);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(0f,0f,-0.2f);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(0.2f,0f,0f);
+		}
+	}
+}
