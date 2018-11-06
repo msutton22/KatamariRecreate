83808e5c1dac650b389aad482940c708b1c08346
Michael Cleary <swim2see@gmail.com>
2018-11-06 10:20:41 -0500

    UI elements- fledgling timer, ball size reader, and object display

---
+++ Assets/LastObjectDisplay.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LastObjectDisplay : MonoBehaviour
+{
+
+	public GameObject objectSpawner;
+	public GameObject cube;
+	public GameObject cylinder;
+	public GameObject plane;
+	public GameObject cubePrefab;
+	public GameObject currentObject;
+
+	public Text lastObjectText;
+
+	public bool displayEmpty;
+
+	private float rotateSpeed = 25f;
+	
+	
+	
+	
+
+	// Use this for initialization
+	void Start ()
+	{
+		displayEmpty = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+		if (displayEmpty == false)
+		{
+			currentObject.transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			WipeList();
+			lastObjectText.text = "Cube";
+			cube.gameObject.SetActive(true);
+		}
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			WipeList();
+			lastObjectText.text = "Cylinder";
+			cylinder.gameObject.SetActive(true);
+		}
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			WipeList();
+			lastObjectText.text = "Plane";
+			plane.gameObject.SetActive(true);
+		}
+	}
+
+	public void WipeList()
+	{
+		lastObjectText.text = " ";
+		cube.gameObject.SetActive(false);
+		cylinder.gameObject.SetActive(false);
+		plane.gameObject.SetActive(false);
+	}
+}
---
+++ Assets/UI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI : MonoBehaviour
+{
+
+	public Image ballSize;
+	public SphereCollider katCollider;
+	public GameObject katamariBall;
+	public float katRadius;
+
+	public GameObject clockHand;
+
+
+	private float timeLeft = 360 / 20;
+
+	// Use this for initialization
+	void Start () {
+		//ballSize.rectTransform.localScale = new Vector3(4,4,1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		katRadius = katCollider.radius;
+		
+		clockHand.transform.Rotate(0f,0f, -Time.deltaTime); 
+		
+		ballSize.rectTransform.localScale = new Vector3(katamariBall.transform.localScale.x/4,katamariBall.transform.localScale.y/4,katamariBall.transform.localScale.z);
+	}
+}
